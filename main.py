5fce6bb9bd9229ce5dc586377609daa59fe24b10
Bartłomiej Ryba <bartlomiejrybaa@gmail.com>
2021-01-26 15:44:22 +0100

    code update

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from ursina import *
+from ursina.prefabs.first_person_controller import FirstPersonController
 from random import randrange
 import math
 
@@ -10,12 +11,15 @@ def calculate_points_distance(point_one, point_two):
     ys = (point_one[1] - point_two[1])**2
     zs = (point_one[2] - point_two[2])**2
 
+    # print (point_one)
+    # print (point_two)
+
     return math.sqrt(xs + ys + zs)
 
 def was_disease_caught(disease_poses, snake_pos, lvl_number):
     for i in range(len(disease_poses)):
         if (calculate_points_distance(disease_poses[i].position, snake_pos) <= 3):
-            return 0.05
+            return 0.5
     return 0
 
 class Diseases(Entity):
@@ -45,30 +49,27 @@ class Snake:
         self.add_new_snake_element(self.snake_elements_positions[0])
         
         self.direction = Vec3(0, 0, 0)
-        self.speed = 4
         self.frame_counter = 0
     
     def move_snake(self):
-        self.frame_counter = self.frame_counter + 1.5
-        if not self.frame_counter % self.speed:
+            
+        for key, value in held_keys.items():
+            if value != 0:
+                if key == 'a' or key == 'left arrow':
+                    self.direction = Vec3(-1, 0, 0) * time.dt * 10
+                elif key == 'd' or key == 'right arrow':
+                    self.direction = Vec3(1, 0, 0) * time.dt * 10
+                elif key == 'w' or key == 'up arrow': 
+                    self.direction = Vec3(0, 1, 0) * time.dt * 10 
+                elif key == 's' or key == 'down arrow':
+                    self.direction = Vec3(0, -1, 0) * time.dt * 10
+                break
 
-            for key, value in held_keys.items():
-                if value != 0:
-                    if key == 'a' or key == 'left arrow':
-                        self.direction = Vec3(-1, 0, 0)
-                    elif key == 'd' or key == 'right arrow':
-                        self.direction = Vec3(1, 0, 0)
-                    elif key == 'w' or key == 'up arrow':
-                        self.direction = Vec3(0, 1, 0)
-                    elif key == 's' or key == 'down arrow':
-                        self.direction = Vec3(0, -1, 0)
-                    break
+        self.snake_elements_positions.append(self.snake_elements_positions[-1] + self.direction)
+        self.snake_elements_positions = self.snake_elements_positions[ -self.snake_elements:]
 
-            self.snake_elements_positions.append(self.snake_elements_positions[-1] + self.direction)
-            self.snake_elements_positions = self.snake_elements_positions[ -self.snake_elements:]
-
-            for snake_el, snake_el_position in zip(self.snake_elements_arr, self.snake_elements_positions):
-                snake_el.position = snake_el_position
+        for snake_el, snake_el_position in zip(self.snake_elements_arr, self.snake_elements_positions):
+            snake_el.position = snake_el_position
 
     def add_new_snake_element(self, position):
         entity = Entity(model='cube', color=color.green, position=position)
@@ -96,14 +97,17 @@ class Game(Ursina):
 
         lvl_info_tet = f"Current level: {self.current_lvl}"
         self.text2 = Text(origin=(-0.5,11), text=lvl_info_tet, scale=0.05)
+
+        self.text3 = Text(origin=(-0.5,11), text=lvl_info_tet, scale=0.05)
     
-        scene.clear()
+        scene.clear() # comment to show ursina info and hide game text fields
         
         Entity(model='plane', scale=self.MAP_SIZE, position=(self.MAP_SIZE / 2, self.MAP_SIZE / 2, 0))
         Entity(model=Grid(self.MAP_SIZE, self.MAP_SIZE), scale=self.MAP_SIZE, position=(self.MAP_SIZE / 2, self.MAP_SIZE / 2, -0.001), color=color.black33)
 
         self.health = HealthIncrement(self.MAP_SIZE, model='cube', color=color.blue)
         self.snake = Snake(self.MAP_SIZE)
+        self.diseases = []
         self.initialize_diseases()
 
         self.health_value = 100
@@ -114,10 +118,9 @@ class Game(Ursina):
         camera.rotation_x = -65
 
     def initialize_diseases(self):
-        self.diseases = []
         add_3_with_each_lvl = 3
         for n in range(add_3_with_each_lvl):
-            self.diseases.append(Diseases(self.MAP_SIZE, model='cube', color=color.red))
+            self.diseases.append(Diseases(self.MAP_SIZE, model='cube', color=color.red, rotation=Vec3(45,45,45)))
        
     def update_info_text(self):
 
@@ -130,14 +133,24 @@ class Game(Ursina):
         if self.text2:
             destroy(self.text2)
 
-        lvl_info_tet = f"Current level: {self.current_lvl}"
-        self.text2 = Text(origin=(-0.5,11), text=lvl_info_tet, scale=0.05)
+        lvl_info_text = f"Current level: {self.current_lvl}"
+        self.text2 = Text(origin=(-0.5,11), text=lvl_info_text, scale=0.05)
+
+    def show_finish_game_info(self):
+
+        if self.text3:
+            destroy(self.text3)
+        end_info_text = f"End game points: {self.current_lvl * self.health_value}"
+        self.text3 = Text(origin=(-0.5,15), text=end_info_text, scale=0.05)
 
     def is_game_over(self):
         snake = self.snake.snake_elements_positions
         if 0 < snake[0][0] and snake[0][0] < self.MAP_SIZE and 0 < snake[0][1] and snake[0][1] < self.MAP_SIZE:
             return
+        
+        self.show_finish_game_info()
         exit()
+
 
     def update(self):
 
@@ -149,8 +162,14 @@ class Game(Ursina):
                 self.update_info_text()   
                 break
 
-        # gathering health
-        if self.snake.snake_elements_positions[-1] == self.health.position:
+        was_health_found = calculate_points_distance(self.snake.snake_elements_positions[-1], self.health.position)
+        print(was_disease_caught)
+        if was_health_found <= 1:
+            # self.health_value -= snake_sickness_points      
+            # self.update_info_text()   
+
+        # # gathering health
+        # if self.snake.snake_elements_positions[-1] == self.health.position:
             self.current_lvl += 1
             self.health_value = self.health_value + 5* self.current_lvl
             self.snake.improve_snake()
